bdbabfef886f270ca4f0f638359faa3bd6c657e2
TURBOHYPER <94942621+TURBOHYPER@users.noreply.github.com>
2022-01-23 13:29:28 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'CatBot created by *Turbo*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
@@ -36,7 +36,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'CatBot created by *Basil sir*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'CatBot created by *Turbo*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,'ʜʏ ᴅᴜᴅᴇ....👋🏻\n\n```MY OWNER:``` *'+Config.OWNER + '*\n\n ```OWNER NUMBER``` *'+Config.BSL + '\n\n---------------------', MessageType.text);
